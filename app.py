076411c21dd7eca828ca42917729f01e7a206dd8
consultants69 <trials.9999@gmail.com>
2024-09-26 18:21:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, make_response, flash
+from random import sample, choice, choices, shuffle, randint, random
 from io import StringIO
-from random import sample, choices, shuffle
 import pandas as pd
 import csv
 
@@ -10,15 +10,21 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route("/")
 def home():
     return render_template("temp.html")
+
 @app.route('/data', methods=['POST', 'GET'])
 def generate_random_userdata_csv():
     fields = {}
+
     if request.method == 'POST' or request.method == 'GET':
         firstname = request.form.get("firstname")
         lastname = request.form.get("lastname")
         age = request.form.get("Age")
         state = request.form.get("states")
+        email = request.form.get("email")
+        gender = request.form.get("gender")
+        occupation = request.form.get("occupation")
         number_of_data = int(request.form.get("data"))
+
         if firstname == None:
             print("First name unchecked")
         else:
@@ -26,6 +32,7 @@ def generate_random_userdata_csv():
                 firstnames = [line.capitalize().strip() for line in file]
                 random_firstnames = sample(firstnames, number_of_data)
                 fields["First Name"] = random_firstnames
+        
         if lastname == None:
             print("Last name unchecked")
         else:
@@ -33,6 +40,7 @@ def generate_random_userdata_csv():
                 lastnames = [line.capitalize().strip() for line in file]
                 random_lastnames = choices(lastnames, k=number_of_data)
                 fields["Last Name"] = random_lastnames
+        
         if age == None:
             print("Age Unchecked")
         else:
@@ -40,6 +48,20 @@ def generate_random_userdata_csv():
             shuffle(random_age)
             random_age.extend(choices(random_age, k=number_of_data-42))
             fields["Age"] = random_age
+        
+        if gender == None:
+            print("Gender Unchecked")
+        else:
+            values = ['M', 'F']
+            result = []
+            for _ in range(number_of_data):
+                if random() < 0.01:
+                    result.append("Others")
+                else:
+                    result.append(choice(values))
+            shuffle(result)
+            fields["Gender"] = result
+        
         if state == None:
             print("State Unchecked")
         else:
@@ -48,7 +70,30 @@ def generate_random_userdata_csv():
                 shuffle(statenames)
                 random_states = choices(statenames, k=number_of_data)
                 fields["State"] = random_states
+        
+        if email == None:
+            print("Email")
+        else:
+            if firstname == None:
+                with open("firstname.txt", "r") as file:
+                    firstnames = [line.capitalize().strip() for line in file]
+                    random_firstnames = sample(firstnames, number_of_data)
+                email = [name+str(randint(10, 500))+"@gmail.com" for name in random_firstnames]
+            else:
+                email = [name+str(randint(10, 500))+"@gmail.com" for name in random_firstnames]
+            fields["Email"] = email
+        
+        if occupation == None:
+            print("Occupation Unchecked")
+        else:
+            with open("occupation.txt", "r") as file:
+                occupations = [line.capitalize().strip() for line in file]
+                shuffle(occupations)
+                random_occupations = choices(occupations, k=number_of_data)
+                fields["Occupation"] = random_occupations
+
         df = pd.DataFrame(fields)
+        
         if not df.empty:
             si = StringIO()
             cw = csv.writer(si)
